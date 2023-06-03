591b7dc7d054963f83e9ff5322a4e31b9f9bd3f0
9Neil259 <104685522+9Neil259@users.noreply.github.com>
2023-06-03 14:03:21 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,31 @@ const redis = require('redis');
 const bodyParser = require('body-parser');
 
 const app = express();
+// Create a new Redis client instance
+const client = redis.createClient({
+  host: 'localhost',
+  port: 6379,
+  // Add any additional configuration options here
+});
+
+// Handle any connection errors
+client.on('error', (error) => {
+  console.error('Redis connection error:', error);
+});
+
+// Start the server after the Redis client is connected
+client.on('ready', () => {
+  app.listen(3001, () => {
+    console.log('Server started on port 3001');
+  });
+});
 
 app.use(bodyParser.json());
 
 // Definieren der Route für den Endpunkt "/events"
 app.get('/events', (req, res) => {
   // Abrufen der Events aus Redis
-  const client = redis.createClient();
+ 
   client.get('events', (err, data) => {
     if (err) {
       console.error(err);
@@ -59,7 +77,10 @@ app.post('/events', (req, res) => {
   });
 });
 
-// Starten des Servers
-app.listen(3000, () => {
-  console.log('Server gestartet auf Port 3000');
+process.on('SIGINT', () => {
+  // Close the Redis client before shutting down the server
+  client.quit(() => {
+    console.log('Redis client closed');
+    process.exit(0);
+  });
 });
