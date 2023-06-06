181e125611897f5563ac09ee32f149b5a2f8b5a7
9Neil259 <104685522+9Neil259@users.noreply.github.com>
2023-06-06 14:41:46 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const redis = require('redis');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 // Create a new Redis client instance
 const client = redis.createClient({
   host: 'localhost',
   port: 6379,
   // Add any additional configuration options here
 });
-
+client.connect();
 // Handle any connection errors
 client.on('error', (error) => {
   console.error('Redis connection error:', error);
@@ -17,6 +19,7 @@ client.on('error', (error) => {
 
 // Start the server after the Redis client is connected
 client.on('ready', () => {
+  console.log('Redis client connected');
   app.listen(3001, () => {
     console.log('Server started on port 3001');
   });
@@ -24,30 +27,30 @@ client.on('ready', () => {
 
 app.use(bodyParser.json());
 
-// Definieren der Route für den Endpunkt "/events"
+// Define the route for the "/events" endpoint
 app.get('/events', (req, res) => {
-  // Abrufen der Events aus Redis
- 
+  // Retrieve events from Redis
+
   client.get('events', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Fehler beim Abrufen der Events' });
     } else if (data) {
-      // Wenn Events bereits im Cache vorhanden sind, sende sie als JSON-Response
+      // If events are already present in the cache, send them as a JSON response
       res.json(JSON.parse(data));
     } else {
-      // Wenn Events nicht im Cache vorhanden sind, führe die Datenbankabfrage durch
-      // und speichere die Events im Cache
+      // If events are not present in the cache, perform the database query
+      // and store the events in the cache
       const events = [
         { id: 1, name: 'Abend', date: '04.02.00', location: 'Graz' },
         { id: 2, name: 'Event 2', date: '05.06.57', location: 'Wien' },
         { id: 3, name: 'Event 3', date: '07.01.74', location: 'Graz' },
       ];
 
-      // Speichern der Events im Cache
+      // Store the events in the cache
       client.set('events', JSON.stringify(events));
 
-      // Senden der Events als JSON-Response
+      // Send the events as a JSON response
       res.json(events);
     }
   });
@@ -55,11 +58,10 @@ app.get('/events', (req, res) => {
 
 app.post('/events', (req, res) => {
   const event = req.body;
-  // Generieren einer eindeutigen ID für das Event
+  // Generate a unique ID for the event
   event.id = Date.now();
 
-  // Speichern des Events im Cache
-  const client = redis.createClient();
+  // Store the event in the cache
   client.get('events', (err, data) => {
     if (err) {
       console.error(err);
